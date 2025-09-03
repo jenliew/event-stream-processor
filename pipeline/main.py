e67b6905d38feafb2fc1e9e3bdcf5ec76ce018b5
Jenny Liew <liew.jenny@gmail.com>
2025-09-03 11:52:32 +0100

    Adding files

---
+++ pipeline/main.py
@@ -0,0 +1,132 @@
+import argparse
+import asyncio
+import json
+import logging
+import os
+import traceback
+from csv import DictReader
+
+from pipeline.src.constant import (
+    DEFAULT_RATE_LIMIT,
+    DEFAULT_URL_API_STR,
+    DEFAULT_WORKER_NUM,
+)
+from pipeline.src.exception import SubmissionError
+from pipeline.src.operational import WorkerManager
+from pipeline.src.record import Record
+from pipeline.src.utils import convert_str_dsp_record
+
+
+def configure_logging(log_level: str):
+    logging.basicConfig(
+        level=getattr(logging, log_level.upper(), logging.INFO),
+        format=(
+            "%(asctime)s - %(levelname)s - %(threadName)s "
+            "- %(name)s - %(message)s"
+        ),
+    )
+    return logging.getLogger("dsp_submission")
+
+
+def process_file_input(file_path: str, record, logger):
+    if not os.path.isfile(file_path):
+        logger.error(f"Input file not found: {file_path}")
+        raise FileNotFoundError(f"{file_path} does not exist.")
+
+    with open(file_path) as input_obj:
+        for row in DictReader(input_obj):
+            try:
+                convert_str_dsp_record(record, row)
+            except SubmissionError as e:
+                raise e
+
+
+def process_line_input(line_str: str, record):
+    try:
+        data = json.loads(line_str)
+        convert_str_dsp_record(record, data)
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Invalid JSON input for --line: {e}")
+    except SubmissionError as e:
+        raise e
+
+
+def main():
+    parser = intialise_scripts()
+    args = parser.parse_args()
+
+    logger = configure_logging(args.log)
+
+    record = Record()
+
+    if args.file:
+        process_file_input(args.file, record, logger)
+    elif args.line:
+        process_line_input(args.line, record)
+    else:
+        logger.error(
+            "Missing --file or --line argument. No campaign data provided."
+        )
+        return
+
+    logger.info(
+        "Successsfully process input data. Preparing "
+        "to submit the jobs to the queue."
+    )
+
+    if record.campaign_record:
+        operator_worker = WorkerManager(
+            record.campaign_record, args.endpoint, args.rate_limit, args.worker
+        )
+
+        asyncio.run(operator_worker.start())
+
+
+def intialise_scripts():
+    parser = argparse.ArgumentParser(description="Submission Campaign data")
+
+    parser.add_argument(
+        "--log",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Enabled logging",
+        default="INFO",
+    )
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "--line", type=str, help="Dict of campaign data to process"
+    )
+
+    group.add_argument(
+        "--file", type=str, help="Full path for the CSV file of campaign data"
+    )
+    parser.add_argument(
+        "--rate-limit",
+        type=int,
+        dest="rate_limit",
+        help="Rate Limit to restrict the request limit to the DSP Endpoint",
+        default=DEFAULT_RATE_LIMIT,
+    )
+    parser.add_argument(
+        "--endpoint",
+        type=str,
+        help="The URL of the DSP API endpoint.",
+        default=DEFAULT_URL_API_STR,
+    )
+    parser.add_argument(
+        "--worker",
+        type=int,
+        help="The number of worker to the queue..",
+        default=DEFAULT_WORKER_NUM,
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except Exception as e:
+        print(traceback.format_exc())
+        print(f"exception caught: {e}")
---
+++ pipeline/src/utils.py
@@ -0,0 +1,69 @@
+import logging
+
+from pipeline.src.constant import (
+    AD_GROUP_KEY_MAPPING,
+    ADS_KEY_MAPPING,
+    CAMPAIGN_KEYS,
+)
+from pipeline.src.exception import SubmissionError
+
+logger = logging.getLogger(__name__)
+
+
+def convert_str_dsp_record(record, input_data):
+
+    grouped_ad_group_data = {}
+    ads_group_data = {}
+
+    campaign_dict = {k: v for k, v in input_data.items() if k in CAMPAIGN_KEYS}
+
+    ad_groups_dict_list = input_data.get("ad_groups", [])
+    ads_dict_list = []
+
+    if not record:
+        raise SubmissionError("Record object didn't create properly.")
+
+    record.add_campaign_record(campaign_dict)
+    if not ad_groups_dict_list:
+
+        for key, value in input_data.items():
+            for ag_key, ag_mapped_key in AD_GROUP_KEY_MAPPING.items():
+                if ag_key in key:
+                    grouped_ad_group_data[ag_mapped_key] = value
+
+            for ad_key, ad_mapped_key in ADS_KEY_MAPPING.items():
+                if ad_key in key:
+                    ads_group_data[ad_mapped_key] = value
+
+        ads_dict_list.append(ads_group_data)
+        grouped_ad_group_data["ads"] = ads_dict_list
+        ad_groups_dict_list.append(grouped_ad_group_data)
+
+    else:
+        for ads_item in ad_groups_dict_list:
+            ads_dict_list += ads_item.get("ads", [])
+
+    for ads_group_items in ad_groups_dict_list:
+        record.insert_ad_groups(
+            campaign_dict["campaign_id"],
+            ads_group_items,
+        )
+
+    return
+
+
+def split_jobs(jobs, num_parts):
+    total_jobs = len(jobs)
+    avg_chunk_size = total_jobs // num_parts
+    job_splits = []
+
+    # First (num_parts - 1) chunks with size avg_chunk_size
+    for i in range(num_parts - 1):
+        start = i * avg_chunk_size
+        end = (i + 1) * avg_chunk_size
+        job_splits.append(jobs[start:end])
+
+    # Last chunk includes the remaining jobs
+    job_splits.append(jobs[(num_parts - 1) * avg_chunk_size :])
+
+    return job_splits
